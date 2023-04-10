86cb878fdc01fd9e2e982f02c38fe362eb39c603
Jiaying Liao <77644190+uliaLiao@users.noreply.github.com>
2023-04-10 10:58:04 -0700

    Create whatever.R

---
+++ whatever.R
@@ -0,0 +1,12 @@
+#' Whatever dataset
+#'
+#' A dataset containing information about whatever
+#'
+#' @format A data frame with ??? rows and ??? variables:
+#'
+#' @name whatever
+#'
+#' @export
+#'
+#' @keywords datasets
+load("data/whatever.rda")
